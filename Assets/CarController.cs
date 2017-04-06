6afe804726e38b62c3058b82601dca1b7771cb16
Norbert Skurnóg <norbert108@gmail.com>
2017-04-06 02:53:45 +0200

    Working truck

--- Assets/CarController.cs
+++ Assets/CarController.cs
@@ -1,72 +1,38 @@
-﻿using System.Collections;
+﻿using UnityEngine;
 using System.Collections.Generic;
-using UnityEngine;
 
-public class CarController : MonoBehaviour {
+public class CarController: MonoBehaviour
+{
+    public List<AxleInfo> axleInfos; // the information about each individual axle
+    public float maxMotorTorque; // maximum torque the motor can apply to wheel
+    public float maxSteeringAngle; // maximum steer angle the wheel can have
 
-    public Transform[] wheels;
- 
-    float enginePower = 150.0f;
-    float power = 0.0f;
-    float brake = 0.0f;
-    float steer = 0.0f;
-    float maxSteer = 25.0f;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        power = Input.GetAxis("Vertical") * enginePower * Time.deltaTime * 250.0f;
-        steer = Input.GetAxis("Horizontal") * maxSteer;
-        brake = Input.GetKey("space") ? GetComponent<Rigidbody>().mass * 0.1f : 0.0f;
-        Debug.Log(power);
-        GetCollider(0).steerAngle = steer;
-        GetCollider(1).steerAngle = steer;
-
-        foreach(var wheel in wheels) {
-            UpdateWheel(wheel);
-        }
-
-
-        if (brake > 0.0)
-        {
-            GetCollider(0).brakeTorque = brake;
-            GetCollider(1).brakeTorque = brake;
-            GetCollider(2).brakeTorque = brake;
-            GetCollider(3).brakeTorque = brake;
-            GetCollider(2).motorTorque = 0.0f;
-            GetCollider(3).motorTorque = 0.0f;
-        }
-        else
-        {
-            GetCollider(0).brakeTorque = 0;
-            GetCollider(1).brakeTorque = 0;
-            GetCollider(2).brakeTorque = 0;
-            GetCollider(3).brakeTorque = 0;
-            GetCollider(2).motorTorque = -power;
-            GetCollider(3).motorTorque = -power;
-        }
-    }
-
-    void UpdateWheel(Transform wheel)
+    public void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.A))
+        float motor = maxMotorTorque * Input.GetAxis("Vertical");
+        float steering = maxSteeringAngle * Input.GetAxis("Horizontal");
+
+        foreach (AxleInfo axleInfo in axleInfos)
         {
-
-            wheel.Rotate(-Vector3.up * Time.deltaTime * power);
-        }
-
-        else if (Input.GetKey(KeyCode.D))
-        {
-
-            wheel.Rotate(Vector3.up * Time.deltaTime * power);
+            if (axleInfo.steering)
+            {
+                axleInfo.leftWheel.steerAngle = steering;
+                axleInfo.rightWheel.steerAngle = steering;
+            }
+            if (axleInfo.motor)
+            {
+                axleInfo.leftWheel.motorTorque = motor;
+                axleInfo.rightWheel.motorTorque = motor;
+            }
         }
     }
+}
 
-    WheelCollider GetCollider(int n) {
-        return wheels[n].gameObject.GetComponent<WheelCollider>();
-    }
+[System.Serializable]
+public class AxleInfo
+{
+    public WheelCollider leftWheel;
+    public WheelCollider rightWheel;
+    public bool motor; // is this wheel attached to motor?
+    public bool steering; // does this wheel apply steer angle?
 }
